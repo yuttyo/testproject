d29123689083005aa276b0b5b16a599cb1a2fc0c
yuttyo <tasogare_souen@yahoo.co.jp>
2020-03-23 21:33:22 +0900

    code6

---
+++ EditTask.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditTask extends CreateTask
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $attributes = parent::attributes();
+
+        return $rule + [
+            'status' => '状態',
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rule = parent::rules();
+
+        $status_rule = Rule::in(array_keys(Task::STATUS));
+
+        return $rule +[
+            'status' => 'required|' . $status_rule,
+            //
+        ];
+    }
+
+    public function messages()
+    {
+        $messages = parent::messages();
+
+        $status_labels = array_map(function($item) {
+            return $item['label'];
+        }, Task::STATUS);
+
+        $status_labels = implode('、', $status_labels);
+
+        return $messages + [
+            'status.in' => ':attribute には ' . $status_labels. ' のいずれかを指定してください。',
+        ];
+    }
+}
